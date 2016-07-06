7127810b4a289ed55a749b17cb3c3afca4dff57d
kingsofcoding <simplecooldude1@gmail.com>
2016-07-06 16:30:17 +0000

    src

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var storage = require('./src/storage');
+var view = require('./src/view');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/views/public'));
@@ -12,6 +14,16 @@ app.get('/', function(req, res) {
 
 app.get('/form', function(req, res) {
    res.sendFile(path.join(__dirname + "/views/form/form.html")); 
+});
+
+app.post('/new', function(req, res) {
+   var body = req.body,
+       title = body.title,
+       content = body.content;
+});
+
+app.get('/:key', function(req, res) {
+    
 });
 
 app.listen(process.env.PORT, function (req, res) {
