3446b1016962f80e9f841c5957ab2b8f30dbc04f
kingsofcoding <simplecooldude1@gmail.com>
2016-07-06 05:09:06 +0000

    boilerplate

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + "/views/index.html"));
+});
 
 app.listen(process.env.PORT, function (req, res) {
     console.log("Log is Listening");
