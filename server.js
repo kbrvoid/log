1ec6d1f03848a2badb12a0435c998432a8d1d1fb
kingsofcoding <simplecooldude1@gmail.com>
2016-07-06 15:30:54 +0000

    public static

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/views/public'));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + "/views/index.html"));
 });
