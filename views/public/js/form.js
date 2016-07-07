cddf98cee66d68ebeb5f074337db2cc0c11ab160
kingsofcoding <simplecooldude1@gmail.com>
2016-07-07 04:12:38 +0000

    textarea instead of content editable

--- views/public/js/form.js
+++ views/public/js/form.js
@@ -2,8 +2,22 @@
 //     document.getElementById("content").value = marked(document.getElementById("content").value);
 // });
 
-function onSub() {
-    document.getElementById("contentHidden").value =  
-        document.getElementById("content").innerHTML;
-    return true;
+// function onSub() {
+//     document.getElementById("contentHidden").value =  
+//         document.getElementById("content").innerHTML;
+//     return true;
+// }
+
+var textarea = document.getElementById('content');
+
+textarea.addEventListener('keydown', autosize);
+             
+function autosize(){
+  var el = this;
+  setTimeout(function(){
+    el.style.cssText = 'height:auto; padding:0';
+    // for box-sizing other than "content-box" use:
+    // el.style.cssText = '-moz-box-sizing:content-box';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  },0);
 }
