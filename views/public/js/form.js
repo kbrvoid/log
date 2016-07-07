05b5809f36172af7c2044630e1a785ab8ce71f97
kingsofcoding <simplecooldude1@gmail.com>
2016-07-07 04:54:49 +0000

    better autosize textarea

--- views/public/js/form.js
+++ views/public/js/form.js
@@ -8,16 +8,39 @@
 //     return true;
 // }
 
-var textarea = document.getElementById('content');
+(function(document) {
 
-textarea.addEventListener('keydown', autosize);
-             
-function autosize(){
-  var el = this;
-  setTimeout(function(){
-    el.style.cssText = 'height:auto; padding:0';
-    // for box-sizing other than "content-box" use:
-    // el.style.cssText = '-moz-box-sizing:content-box';
-    el.style.cssText = 'height:' + el.scrollHeight + 'px';
-  },0);
-}
+var flex = {
+    tagName: 'TEXTAREA',
+    attribute: 'autosize',
+    buffer: 500,
+    events: 'input propertychange change',
+    adjust: function(el, shrunk) {
+        var height = el.scrollHeight,
+            style = el.style;
+        if (height > el.offsetHeight - 2) {
+            style.height = (height+flex.buffer) + 'px';
+        } else if (!shrunk) {
+            style.height = '';
+            flex.adjust(el, true);
+        }
+
+        var width = el.scrollWidth;
+        if (width > el.offsetWidth) {
+            style.width = (width+flex.buffer) + 'px';
+        } else if (!shrunk) {
+            style.width = '';
+            flex.adjust(el, true);
+        }
+    }
+};
+flex.events.split(' ').forEach(function(type) {
+    document.addEventListener(type, function(e) {
+        var el = e.target;
+        if (el.tagName === flex.tagName && el.hasAttribute(flex.attribute)) {
+            flex.adjust(el);
+        }
+    });
+});
+
+})(document);
